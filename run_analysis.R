c64680e2a1bf4f8c4963968a92f4801ca520842a
samuzaffar99 <sam.muzaffar99@gmail.com>
2020-06-15 11:48:04 +0500

    completed basic analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,61 @@
-# setwd("C:\\Users\\cools\\Desktop\\R\\Course3\\Week4\\Project\\UCI HAR Dataset\\test")
-# TestFiles <- list.files(pattern='*.txt', recursive=T)
-# setwd("C:\\Users\\cools\\Desktop\\R\\Course3\\Week4\\Project\\UCI HAR Dataset\\train")
-# TrainFiles <- list.files(pattern='*.txt', recursive=T)
-
-
+library(dplyr)
 # Read all the Data
 dir <- "C:\\Users\\cools\\Desktop\\R\\Course3\\Week4\\Project\\UCI HAR Dataset\\"
-# Test Data
-test_sub <- read.table(paste0(dir,"test\\subject_test.txt"))
-test_X   <- read.table(paste0(dir,"test\\subject_test.txt"))
-test_Y   <- read.table(paste0(dir,"test\\Y_test.txt"))
+activity <- read.table(paste0(dir,"activity_labels.txt"),col.names = c("index","activity"))
+features   <- read.table(paste0(dir,"features.txt"),col.names = c("index","feature"))
 
 # Training Data
 train_sub <- read.table(paste0(dir,"train\\subject_train.txt"))
-train_X   <- read.table(paste0(dir,"train\\subject_train.txt"))
-train_Y   <- read.table(paste0(dir,"train\\Y_train.txt"))
+train_X   <- read.table(paste0(dir,"train\\X_train.txt"))
+train_y   <- read.table(paste0(dir,"train\\y_train.txt"))
 
-# Combine Test and Training Data
-sub <- rbind(test_sub,train_sub)
-X   <- rbind(test_X,train_X)
-Y   <- rbind(test_Y,train_Y)
+# Test Data
+test_sub <- read.table(paste0(dir,"test\\subject_test.txt"))
+test_X   <- read.table(paste0(dir,"test\\X_test.txt"))
+test_y   <- read.table(paste0(dir,"test\\y_test.txt"))
 
-rm(test_sub,test_X,test_Y)
-rm(train_sub,train_X,train_Y)
+# Combine Training and Test Data
+sub <- rbind(train_sub,test_sub)
+X   <- rbind(train_X,test_X)
+y   <- rbind(train_y,test_y)
 
+rm(train_sub,train_X,train_y)
+rm(test_sub,test_X,test_y)
 
 # Combine into Dataframe by columns
-Data <- cbind(sub,X,Y)
-names(Data) <-c("Subject","X","Y")
+Data <- cbind(sub,y,X)
+names(Data) <-c("Subject","Activity",as.character(features$feature))
 
-rm(sub,X,Y)
+# rm(sub,X,y)
+# Only mean and std measurements extracted //remove others
+Data <- Data %>% select(Subject,Activity,contains("mean"),contains("std"))
 
-Means <- colMeans(Data)
+
+# Replace Activity ID with Activity Name
+Data$Activity <- activity$activity[Data$Activity]
+
+# Label Data Set with descriptive names
+names(Data) <- gsub("Body","Body",names(Data))
+names(Data) <- gsub("Gravity","Gravity",names(Data),ignore.case = T)
+names(Data) <- gsub("Acc","Accelerometer",names(Data))
+names(Data) <- gsub("Gyro","Gyroscope",names(Data))
+names(Data) <- gsub("Jerk","Jerk",names(Data))
+names(Data) <- gsub("Mag","Magnitude",names(Data))
+
+names(Data) <- gsub("mean","Mean",names(Data))
+names(Data) <- gsub("std","SD",names(Data))
+names(Data) <- gsub("Freq","Frequency",names(Data))
+
+names(Data) <- gsub("^t","Time",names(Data))
+names(Data) <- gsub("^f","Frequency",names(Data))
+names(Data) <- gsub("\\(t","\\(Time",names(Data))
+
+names(Data) <- gsub("angle","Angle",names(Data))
+names(Data) <- gsub("-","",names(Data))
+names(Data) <- gsub("\\),",",",names(Data))
+
+# names(Data)
+
+# Create and Export New Data with averages for each activity and subject
+Data2 <- Data %>% group_by(Subject, Activity) %>% summarise_all(mean)
+write.table(Data2,"Data2.txt")
